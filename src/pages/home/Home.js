a9bf0c621b31dfe71842bdef6a5d84963fcab376
hr-shuvo <h.r.shuvo9@gmail.com>
2024-11-03 01:27:54 +0600

    add home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,34 @@
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
+import "./Home.scss";
+import loginImg from "./../../assets/login.svg";
 
 const Home = () => {
-    return(
+
+    return (
         <div>
-            <Header ></Header>
-            <h1>Home</h1>
+            <Header></Header>
+            <section className='container hero'>
+                <div className='hero-text'>
+                    <h2>Ultimate MERN Stack Authentication System</h2>
+                    <p>Learn and Master Authentication and Authorization using MERN Stack</p>
+                    <p>Implement User Registration</p>
+
+                    <div className="hero-buttons --flex-start">
+                        <button className="--btn --btn-danger">Register</button>
+                    </div>
+
+                </div>
+                <div className='hero-image'>
+                    <img src={loginImg} alt={'Auth'}/>
+
+                </div>
+
+            </section>
             <Footer></Footer>
         </div>
     );
-  
+
 };
 
 export default Home;
