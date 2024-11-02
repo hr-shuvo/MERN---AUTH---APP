1692eb77f777cfc9ed8aa1cf08e41e37d5acdbff
hr-shuvo <h.r.shuvo9@gmail.com>
2024-11-03 00:44:56 +0600

    route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/home/Home";
 
 
 function App() {
-  return (
-    <div>
-      <h1>Hello World!!</h1>
-
-    </div>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home/>}/>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
