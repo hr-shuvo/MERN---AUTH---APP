6c9c53031c389581070dfd11dbf53d1d04b7b580
hr-shuvo <h.r.shuvo9@gmail.com>
2024-11-03 01:14:13 +0600

    Header and Footer

---
+++ src/components/header/Header.js
@@ -0,0 +1,52 @@
+import "./Header.scss"
+import {IoLogInOutline} from "react-icons/io5";
+import {FaUserCircle} from "react-icons/fa";
+import {Link, NavLink, useNavigate} from "react-router-dom";
+
+const activeLink = ({isActive}) => (isActive ? 'active' : '')
+
+const Header = () => {
+    const navigate = useNavigate()
+
+    const goHome = () =>{
+        navigate('/')
+    }
+
+    return (
+        <header className={'header'}>
+            <nav>
+                <div className="logo" onClick={goHome}>
+                    <IoLogInOutline size={35}/>
+                    <span>AUTH:Z</span>
+                </div>
+
+                <ul className='home-links'>
+                    <li className='--flex-center'>
+                        <FaUserCircle size={20}/>
+                        <p className='--color-white'>
+                            Hi, Shuvo
+                        </p>
+                    </li>
+                    <li>
+                        <button className='--btn --btn-primary'>
+                            <Link to={'/login'}>Login</Link>
+                        </button>
+                    </li>
+                    <li>
+                        <NavLink className={activeLink} to={'/login'}>
+                            Profile
+                        </NavLink>
+                    </li>
+
+                    <li>
+                        <button className='--btn --btn-secondary'>
+                            Logout
+                        </button>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,12 @@
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+
 const Home = () => {
     return(
         <div>
+            <Header ></Header>
             <h1>Home</h1>
+            <Footer></Footer>
         </div>
     );
   
