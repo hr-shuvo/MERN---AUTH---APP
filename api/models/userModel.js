892d87a8f0fc4eadc988a4515f95c3f0edb010b4
hr-shuvo <h.r.shuvo9@gmail.com>
2024-11-04 05:49:22 +0600

    add user model

---
+++ api/models/userModel.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Please add a name']
+        },
+        email: {
+            type: String,
+            required: [true, 'Please add a email'],
+            unique: true,
+            trim: true,
+            // match: []
+        },
+        password: {
+            type: String,
+            required: [true, 'Please add a password']
+        },
+        photo: {
+            type: String,
+            required: [true, 'Please add a photo'],
+            default: ''
+        },
+        phone: {
+            type: String,
+            default: ''
+        },
+        bio: {
+            type: String,
+            default: ''
+        },
+        role: {
+            type: String,
+            required: true,
+            default: 'subscriber', // author, admin, suspended
+        },
+        isVerified: {
+            type: Boolean,
+            default: false
+        },
+        userAgent: {
+            type: Array,
+            required: true,
+            default: []
+        },
+    },
+    {
+        timestamps: true,
+        minimize: false
+    }
+)
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
