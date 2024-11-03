8cf24c86abb8b3b40b083292cac3802c18032de2
hr-shuvo <h.r.shuvo9@gmail.com>
2024-11-03 09:18:14 +0600

    add search box

--- src/pages/userList/UserList.js
+++ src/pages/userList/UserList.js
@@ -1,16 +1,28 @@
 import './UserList.scss'
 import PageMenu from "../../components/pageMenu/PageMenu";
 import UserStats from "../../components/userStats/UserStats";
+import Search from "../../components/search/Search";
 
 const UserList = () => {
-  return (
-      <section>
-        <div className='container'>
-            <PageMenu />
-            <UserStats />
-        </div>
-      </section>
-  )
+    return (
+        <section>
+            <div className='container'>
+                <PageMenu/>
+                <UserStats/>
+
+                <div className='user-list'>
+                    <div className='--flex-between'>
+                    <span>
+                        <h3>All Users</h3>
+                    </span>
+                        <span><Search/></span>
+
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default UserList;
