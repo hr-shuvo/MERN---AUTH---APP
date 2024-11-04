bb6de5fa4121c2fa1af967ce58d01affab0a65ee
hr-shuvo <h.r.shuvo9@gmail.com>
2024-11-04 07:11:58 +0600

    password hash

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const userSchema = mongoose.Schema(
     {
@@ -49,7 +50,20 @@ const userSchema = mongoose.Schema(
         timestamps: true,
         minimize: false
     }
-)
+);
+
+// Encrypt password before save
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+});
 
 const User = mongoose.model('User', userSchema)
 module.exports = User
