cc78f1f2dc24fdbb93f4b269f1ba0d99d7b03a73
hr-shuvo <h.r.shuvo9@gmail.com>
2024-11-04 11:43:17 +0600

    user

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -129,7 +129,19 @@ const logoutUser = asyncHandler(async(req, res) =>{
 
 // get user
 const getUser = asyncHandler(async(req, res) =>{
-    res.send('Get user')
+    const user = await User.findById(req.user._id);
+
+    if(user){
+        const {_id, name, email, phone, bio, photo, role, isVerified} = user
+
+        res.status(200).json({
+            _id, name, email, phone, bio, photo, role, isVerified
+        });
+
+    }else{
+        res.status(404)
+        throw new Error('User not found');
+    }
 })
 
 module.exports = {
